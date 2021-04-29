21bbfab5015a72dafc11884e33b39d344624c1f5
lawrence <chenlawrence0220@gmail.com>
2021-04-29 16:17:11 +0800

    Replace the pictures of main page

--- frontend/main.js
+++ frontend/main.js
@@ -2,7 +2,8 @@ var Main = {
   data() {
     return {
       fits: ['contain'],
-      url: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
+      url: 'images/web_icon.jpg',
+      items: ["Explore the world", "Examine our data map", "How many tweets are you producing everyday", "Enjoy the beauty of technology"]
     };
   },
   methods: {
