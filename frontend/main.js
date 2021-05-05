13a0845045932f3102a5ee15637bafd327ca5e93
Man-Xu <59200553+Man-Xu@users.noreply.github.com>
2021-05-05 13:25:32 +1000

    Add files via upload

--- frontend/main.js
+++ frontend/main.js
@@ -1,6 +1,8 @@
 var Main = {
   data() {
     return {
+      // activeIndex: '1',
+      // activeIndex2: '1',
       fits: ['contain'],
       url: 'images/web_icon.jpg',
       items: ["Explore the world", "Examine our data map", "How many tweets are you producing everyday", "Enjoy the beauty of technology"]
